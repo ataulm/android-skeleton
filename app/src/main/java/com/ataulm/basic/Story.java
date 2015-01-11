72651b36e9d923dcfd3301ab91a5f3c7d3f16eef
Ataul Munim <timberwo7ves@gmail.com>
2015-01-11 18:26:49 +0000

    Add story activity

---
+++ app/src/main/java/com/ataulm/basic/Story.java
@@ -0,0 +1,20 @@
+package com.ataulm.basic;
+
+public class Story {
+
+    public static final String LOREM_IPSUM = "<p>But I <a href=\"http://google.com\">must explain</a> to you how all this mistaken idea of " +
+            "denouncing pleasure and " +
+            "praising " +
+            "pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness.</p>\n" +
+            "\n" +
+            "<p>No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful.</p>\n" +
+            "\n" +
+            "<p>Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure.</p>\n" +
+            "\n" +
+            "<p>To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it?</p>\n" +
+            "\n" +
+            "<p>But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?</p>\n" +
+            "\n" +
+            "<p>On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of " +
+            "pleasure of the moment, so blinded by desire, that they cannot foresee</p>";
+}
---
+++ app/src/main/java/com/ataulm/basic/StoryActivity.java
@@ -0,0 +1,33 @@
+package com.ataulm.basic;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.TextView;
+
+public class StoryActivity extends Activity {
+
+    private AccessibilityDependentViewPropertyUpdater viewPropertyUpdater;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_story);
+
+        View storyTextView = findViewById(R.id.story_text_short_1);
+        View storyTextView2 = findViewById(R.id.story_text_short_2);
+        TextView storyTextView3 = (TextView) findViewById(R.id.story_text_lorem_ipsum);
+        storyTextView3.setText(Html.fromHtml(Story.LOREM_IPSUM));
+
+        viewPropertyUpdater =
+                AccessibilityDependentViewPropertyUpdater.newInstance(this, storyTextView, storyTextView2, storyTextView3);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        viewPropertyUpdater.updateFocusablePropertyForNonInteractiveElements();
+    }
+
+}
