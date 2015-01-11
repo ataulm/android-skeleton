bf477704c3dd09719a2c3b1a17d25b6dbb3d86b0
Ataul Munim <timberwo7ves@gmail.com>
2015-01-11 20:52:39 +0000

    Add basic show activity layout, not accessible

---
+++ app/src/main/java/com/ataulm/basic/ShowActivity.java
@@ -0,0 +1,13 @@
+package com.ataulm.basic;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ShowActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show);
+    }
+}
