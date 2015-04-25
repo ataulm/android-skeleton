813c1e1595aa219d7d619a1c9a8588599dea3e6f
Ataul Munim <timberwo7ves@gmail.com>
2015-04-26 00:36:04 +0100

    Add simple GridView to demonstrate crap keyboard nav

--- mobile/src/main/java/com/ataulm/basic/MyActivity.java
+++ mobile/src/main/java/com/ataulm/basic/MyActivity.java
@@ -2,12 +2,14 @@ package com.ataulm.basic;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.annotation.DrawableRes;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.method.KeyListener;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.BaseAdapter;
 import android.widget.TextView;
 
 public class MyActivity extends Activity {
@@ -17,88 +19,43 @@ public class MyActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
 
-        RecyclerView listview = (RecyclerView) findViewById(R.id.listview);
-        listview.setLayoutManager(new LinearLayoutManager(this));
-        listview.setAdapter(new DummyAdapter(new ToastDisplayer(this)));
+        AbsListView listview = (AbsListView) findViewById(R.id.listview);
+        listview.setAdapter(new DummyAdapter(getLayoutInflater()));
     }
 
-    private static class DummyAdapter extends RecyclerView.Adapter<DummyAdapter.ViewHolder> {
+    private static class DummyAdapter extends BaseAdapter {
 
         private static final int COUNT = 25;
 
-        private final ToastDisplayer toastDisplayer;
+        private final LayoutInflater layoutInflater;
 
-        DummyAdapter(ToastDisplayer toastDisplayer) {
-            this.toastDisplayer = toastDisplayer;
+        DummyAdapter(LayoutInflater layoutInflater) {
+            this.layoutInflater = layoutInflater;
         }
 
         @Override
-        public ViewHolder onCreateViewHolder(ViewGroup parent, int i) {
-            return ViewHolder.inflate(parent, toastDisplayer);
-        }
-
-        @Override
-        public void onBindViewHolder(ViewHolder viewHolder, int i) {
-            viewHolder.bind("Item " + i);
-        }
-
-        @Override
-        public int getItemCount() {
+        public int getCount() {
             return COUNT;
         }
 
-        static class ViewHolder extends RecyclerView.ViewHolder {
+        @Override
+        public String getItem(int position) {
+            return "Item " + position;
+        }
 
-            private final TextView titleTextView;
-            private final TextView moreActionOneTextView;
-            private final TextView moreActionTwoTextView;
-            private final ToastDisplayer toastDisplayer;
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
 
-            static ViewHolder inflate(ViewGroup parent, ToastDisplayer toastDisplayer) {
-                LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
-                return new ViewHolder(layoutInflater.inflate(R.layout.dummy_item, parent, false), toastDisplayer);
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view = convertView;
+            if (view == null) {
+                view = layoutInflater.inflate(R.layout.dummy_item, parent, false);
             }
-
-            public ViewHolder(View itemView, ToastDisplayer toastDisplayer) {
-                super(itemView);
-                this.toastDisplayer = toastDisplayer;
-
-                this.titleTextView = (TextView) itemView.findViewById(R.id.text_title);
-                this.moreActionOneTextView = (TextView) itemView.findViewById(R.id.text_more_action_one);
-                this.moreActionTwoTextView = (TextView) itemView.findViewById(R.id.text_more_action_two);
-            }
-
-            void bind(final String text) {
-                titleTextView.setText(text);
-
-                itemView.setOnClickListener(new View.OnClickListener() {
-
-                    @Override
-                    public void onClick(View v) {
-                        toastDisplayer.display("onClick " + text);
-                    }
-
-                });
-
-                moreActionOneTextView.setOnClickListener(new View.OnClickListener() {
-
-                    @Override
-                    public void onClick(View v) {
-                        toastDisplayer.display("onClick " + text + " - action 1");
-                    }
-
-                });
-
-                moreActionTwoTextView.setOnClickListener(new View.OnClickListener() {
-
-                    @Override
-                    public void onClick(View v) {
-                        toastDisplayer.display("onClick " + text + " - action 2");
-                    }
-
-                });
-            }
-
+            ((TextView) view).setText(getItem(position));
+            return view;
         }
 
     }
