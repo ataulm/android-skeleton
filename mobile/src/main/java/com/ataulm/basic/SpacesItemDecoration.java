eaf26ef094db2331d22a70a707725709fa874674
Ataul Munim <timberwo7ves@gmail.com>
2015-05-02 00:21:48 +0100

    add item first or last col check

--- mobile/src/main/java/com/ataulm/basic/SpacesItemDecoration.java
+++ mobile/src/main/java/com/ataulm/basic/SpacesItemDecoration.java
@@ -1,5 +1,6 @@
 package com.ataulm.basic;
 
+import android.graphics.Color;
 import android.graphics.Rect;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
@@ -23,13 +24,45 @@ class SpacesItemDecoration extends RecyclerView.ItemDecoration {
             outRect.bottom = verticalSpacing / 2;
             return;
         }
-
         int position = parent.getChildPosition(view);
 
         outRect.left = horizontalSpacing / 2;
         outRect.top = verticalSpacing / 2;
         outRect.right = horizontalSpacing / 2;
         outRect.bottom = verticalSpacing / 2;
+
+        if (itemIsInFirstColumn(position)) {
+            outRect.left = 0;
+        }
+        if (itemIsInLastColumn(position)) {
+            outRect.right = 0;
+        }
+    }
+
+    private boolean itemIsInFirstColumn(int position) {
+        int spanCount = spanSizeLookup.getSpanCount();
+
+        int spanTally = 0;
+        for (int i = 0; i <= position; i++) {
+            spanTally += spanSizeLookup.getSpanSize(i);
+            if (spanTally == spanCount) {
+                spanTally = 0;
+            }
+        }
+        return spanTally - spanSizeLookup.getSpanSize(position) == 0;
+    }
+
+    private boolean itemIsInLastColumn(int position) {
+        int spanCount = spanSizeLookup.getSpanCount();
+
+        int spanTally = 0;
+        for (int i = 0; i <= position; i++) {
+            spanTally += spanSizeLookup.getSpanSize(i);
+            if (spanTally == spanCount) {
+                spanTally = 0;
+            }
+        }
+        return spanTally == 0;
     }
 
     public interface SpanSizeLookup {
