00edf40c9e729d1debad9789ea334235e3ee74fa
Ataul Munim <timberwo7ves@gmail.com>
2015-05-03 22:57:34 +0100

    Extract Grid abstraction

--- mobile/src/main/java/com/ataulm/basic/SpacesItemDecoration.java
+++ mobile/src/main/java/com/ataulm/basic/SpacesItemDecoration.java
@@ -4,6 +4,9 @@ import android.graphics.Rect;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class SpacesItemDecoration extends RecyclerView.ItemDecoration {
 
     private final int horizontalSpacing;
@@ -18,6 +21,8 @@ class SpacesItemDecoration extends RecyclerView.ItemDecoration {
 
     @Override
     public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+        Grid grid = Grid.newInstance(parent.getAdapter().getItemCount(), spanSizeLookup);
+
         int position = parent.getChildPosition(view);
 
         outRect.left = horizontalSpacing / 2;
@@ -26,22 +31,26 @@ class SpacesItemDecoration extends RecyclerView.ItemDecoration {
         outRect.bottom = verticalSpacing / 2;
 
         StringBuilder place = new StringBuilder();
-        if (itemIsInFirstColumn(position)) {
+        if (grid.itemIsInFirstColumn(position)) {
             outRect.left = 0;
             place.append("fc,");
         }
-        if (itemIsInLastColumn(position)) {
+
+        if (grid.itemIsInLastColumn(position)) {
             outRect.right = 0;
             place.append("lc,");
         }
-        if (itemIsInFirstRow(position)) {
+
+        if (grid.itemIsInFirstRow(position)) {
             outRect.top = 0;
             place.append("fr,");
         }
-        if (itemIsInLastRow(position, parent.getAdapter().getItemCount())) {
+
+        if (grid.itemIsInLastRow(position)) {
             outRect.bottom = 0;
             place.append("lr,");
         }
+
         if (place.length() > 0) {
             ((ItemView) view).update(place.substring(0, place.length() - 1));
         } else {
@@ -49,68 +58,149 @@ class SpacesItemDecoration extends RecyclerView.ItemDecoration {
         }
     }
 
-    private boolean itemIsInFirstRow(int position) {
-        int spanCount = spanSizeLookup.getSpanCount();
-        int spanTally = 0;
-        for (int i = 0; i <= position; i++) {
-            spanTally += spanSizeLookup.getSpanSize(i);
-            if (spanTally > spanCount) {
-                return false;
+    private static class Grid {
+
+        private final List<Row> rows = new ArrayList<>();
+
+        static Grid newInstance(int adapterCount, SpanSizeLookup spanSizeLookup) {
+            Grid grid = new Grid();
+
+            int maxSpans = spanSizeLookup.getSpanCount();
+            Row tempRow = new Row(maxSpans);
+            for (int i = 0; i < adapterCount; i++) {
+                Cell cell = new Cell(i, spanSizeLookup.getSpanSize(i));
+                if (!tempRow.canFit(cell)) {
+                    grid.add(tempRow);
+                    tempRow = new Row(maxSpans);
+                }
+                tempRow.add(cell);
             }
+            grid.add(tempRow);
+
+            return grid;
         }
-        return true;
+
+        private Grid() {
+            // use the mu'fn factory method
+        }
+
+        void add(Row row) {
+            rows.add(row);
+        }
+
+        boolean itemIsInFirstRow(int adapterPosition) {
+            return rowIndexWith(adapterPosition) == 0;
+        }
+
+        boolean itemIsInLastRow(int adapterPosition) {
+            return rowIndexWith(adapterPosition) == rows.size() - 1;
+        }
+
+        boolean itemIsInFirstColumn(int adapterPosition) {
+            Row row = findRowWith(adapterPosition);
+            return row.itemStartsInFirstColumn(adapterPosition);
+        }
+
+        boolean itemIsInLastColumn(int adapterPosition) {
+            Row row = findRowWith(adapterPosition);
+            return row.itemEndsInLastColumn(adapterPosition);
+        }
+
+        private int rowIndexWith(int adapterPosition) {
+            for (int i = 0; i < rows.size(); i++) {
+                if (rows.get(i).hasCell(adapterPosition)) {
+                    return i;
+                }
+            }
+            throw Row.outOfBounds(adapterPosition);
+        }
+
+        private Row findRowWith(int adapterPosition) {
+            for (Row row : rows) {
+                if (row.hasCell(adapterPosition)) {
+                    return row;
+                }
+            }
+            throw Row.outOfBounds(adapterPosition);
+        }
+
     }
 
-    private boolean itemIsInLastRow(int position, int itemCount) {
-        int spanCount = spanSizeLookup.getSpanCount();
+    private static class Row {
 
-        int rowWithItem = 0;
-        int rowCount = itemCount > 0 ? 1 : 0;
-        int spansInLatestKnownRow = 0;
+        private final List<Cell> cells = new ArrayList<>();
+        private final int rowSize;
 
-        for (int i = 0; i < itemCount; i++) {
-            int spanSize = spanSizeLookup.getSpanSize(i);
-            if (spansInLatestKnownRow + spanSize > spanCount) {
-                rowCount++;
-                spansInLatestKnownRow = spanSize;
-            } else {
-                spansInLatestKnownRow += spanSize;
-            }
-            if (i == position) {
-                rowWithItem = rowCount;
-            }
+        Row(int rowSize) {
+            this.rowSize = rowSize;
         }
-        return rowWithItem == rowCount;
+
+        boolean canFit(Cell cell) {
+            return filledSpans() + cell.size <= rowSize;
+        }
+
+        void add(Cell cell) {
+            cells.add(cell);
+        }
+
+        private int spanStart(int adapterPosition) {
+            for (int i = 0; i < cells.size(); i++) {
+                if (cells.get(i).position == adapterPosition) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        int filledSpans() {
+            int spanCount = 0;
+            for (Cell cell : cells) {
+                spanCount += cell.size;
+            }
+            return spanCount;
+        }
+
+        boolean hasCell(int adapterPosition) {
+            return cellAt(adapterPosition) != null;
+        }
+
+        private Cell cellAt(int adapterPosition) {
+            for (Cell cell : cells) {
+                if (cell.position == adapterPosition) {
+                    return cell;
+                }
+            }
+            return null;
+        }
+
+        boolean itemStartsInFirstColumn(int adapterPosition) {
+            return spanStart(adapterPosition) == 0;
+        }
+
+        boolean itemEndsInLastColumn(int adapterPosition) {
+            Cell cell = cellAt(adapterPosition);
+            if (cell == null) {
+                throw outOfBounds(adapterPosition);
+            }
+            return spanStart(adapterPosition) + cell.size == rowSize;
+        }
+
+        static IndexOutOfBoundsException outOfBounds(int adapterPosition) {
+            return new IndexOutOfBoundsException("Couldn't find adapterPosition: " + adapterPosition);
+        }
+
     }
 
-    private boolean itemIsInFirstColumn(int position) {
-        int spanCount = spanSizeLookup.getSpanCount();
-        int spanTally = 0;
-        for (int i = 0; i <= position; i++) {
-            if (i == position && spanTally == 0) {
-                return true;
-            }
-            spanTally += spanSizeLookup.getSpanSize(i);
-            if (spanTally == spanCount) {
-                spanTally = 0;
-            }
-        }
-        return false;
-    }
+    private static class Cell {
 
-    private boolean itemIsInLastColumn(int position) {
-        int spanCount = spanSizeLookup.getSpanCount();
-        int spansInLatestKnownRow = 0;
-        for (int i = 0; i <= position; i++) {
-            spansInLatestKnownRow += spanSizeLookup.getSpanSize(i);
-            if (i == position && spansInLatestKnownRow == spanCount) {
-                return true;
-            }
-            if (spansInLatestKnownRow == spanCount) {
-                spansInLatestKnownRow = 0;
-            }
+        final int position;
+        final int size;
+
+        Cell(int position, int size) {
+            this.position = position;
+            this.size = size;
         }
-        return false;
+
     }
 
     public interface SpanSizeLookup {
