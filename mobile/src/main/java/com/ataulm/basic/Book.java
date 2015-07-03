6111c787011f20e92c7b6c4f097a92b4261e4f8e
Ataul Munim <timberwo7ves@gmail.com>
2015-07-03 15:13:52 +0100

    adding author name to book

--- mobile/src/main/java/com/ataulm/basic/Book.java
+++ mobile/src/main/java/com/ataulm/basic/Book.java
@@ -5,9 +5,11 @@ import android.support.annotation.StringRes;
 public class Book {
 
     private String bookName;
+    private String authorName;
 
-    public Book(String bookName) {
+    public Book(String bookName, String authorName) {
         this.bookName = bookName;
+        this.authorName = authorName;
     }
 
     public String getBookName() {
@@ -16,5 +18,13 @@ public class Book {
 
     public void setBookName(String bookName) {
         this.bookName = bookName;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
     }
 }
--- mobile/src/main/java/com/ataulm/basic/DummyBookProvider.java
+++ mobile/src/main/java/com/ataulm/basic/DummyBookProvider.java
@@ -6,11 +6,11 @@ import java.util.List;
 class DummyBookProvider implements BookProvider {
 
     private static final List<Book> DUMMY = Arrays.asList(
-            new Book("Harry Potter"), new Book("LOTR1"), new Book("Artemis Fowl"),
-            new Book("Dead Souls"), new Book("Northern Lights"), new Book("Subtle Knife"),
-            new Book("Borrowers"), new Book("The Carpet People"),
-            new Book("Hitchhiker's Guide"), new Book("Carrie"),
-            new Book("Salem's Lot"), new Book("Clean Code"), new Book("Hungry Caterpillar"));
+            new Book("Harry Potter", "sdfasdf"), new Book("LOTR1", "sdfasdf"), new Book("Artemis Fowl", "sdfasdf"),
+            new Book("Dead Souls", "sdfasdf"), new Book("Northern Lights", "sdfasdf"), new Book("Subtle Knife", "sdfasdf"),
+            new Book("Borrowers", "sdfasdf"), new Book("The Carpet People", "sdfasdf"),
+            new Book("Hitchhiker's Guide", "sdfasdf"), new Book("Carrie", "sdfasdf"),
+            new Book("Salem's Lot", "sdfasdf"), new Book("Clean Code", "sdfasdf"), new Book("Hungry Caterpillar", "sdfasdf"));
 
     @Override
     public Book getBook(int position) {
