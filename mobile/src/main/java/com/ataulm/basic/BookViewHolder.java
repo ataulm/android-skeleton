8bdb0821b209af782aa9949f9f8ed41591ad847d
Ataul Munim <timberwo7ves@gmail.com>
2015-07-03 15:37:12 +0100

    added author name to view

--- mobile/src/main/java/com/ataulm/basic/BookViewHolder.java
+++ mobile/src/main/java/com/ataulm/basic/BookViewHolder.java
@@ -9,20 +9,28 @@ import android.widget.TextView;
 final class BookViewHolder extends RecyclerView.ViewHolder {
 
     private final TextView titleView;
+    private final TextView authorView;
 
     public static BookViewHolder inflate(ViewGroup parent, LayoutInflater layoutInflater) {
         View view = layoutInflater.inflate(R.layout.view_book, parent, false);
-        return new BookViewHolder(view, ((TextView) view));
+
+        TextView titleView = (TextView) view.findViewById(R.id.book_name_text);
+        TextView authorView = (TextView) view.findViewById(R.id.book_author_text);
+
+        return new BookViewHolder(view, titleView, authorView);
     }
 
-    private BookViewHolder(View itemView, TextView titleView) {
+    private BookViewHolder(View itemView, TextView titleView, TextView authorView) {
         super(itemView);
         this.titleView = titleView;
+        this.authorView = authorView;
     }
 
     public void bind(final Book book, final BookInteractionsListener listener) {
         titleView.setText(book.getBookName());
-        titleView.setOnClickListener(
+        authorView.setText(book.getAuthorName());
+
+        itemView.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
@@ -30,6 +38,8 @@ final class BookViewHolder extends RecyclerView.ViewHolder {
                     }
                 }
         );
+
+        itemView.setContentDescription(book.getBookName());
     }
 
 }
