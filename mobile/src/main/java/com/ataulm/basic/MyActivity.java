89cd3db3091c27229dab149b723e47344c6b3d6f
Ataul Munim <timberwo7ves@gmail.com>
2015-08-19 22:39:35 +0100

    Add hello world settings

--- mobile/src/main/java/com/ataulm/basic/MyActivity.java
+++ mobile/src/main/java/com/ataulm/basic/MyActivity.java
@@ -1,7 +1,18 @@
 package com.ataulm.basic;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.RadioButton;
 
 public class MyActivity extends Activity {
 
@@ -9,6 +20,46 @@ public class MyActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+    }
+
+    public static class HelloWorldDialogFragment extends DialogFragment {
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.dialog_fragment_hello_world, container);
+        }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            Dialog dialog = super.onCreateDialog(savedInstanceState);
+            dialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+            return dialog;
+        }
+
+    }
+
+    public static class SettingsFragment extends PreferenceFragment {
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.preference_settings_about);
+
+            PreferenceCategory aboutCategory = (PreferenceCategory) findPreference("key_about");
+            Preference loremPref = aboutCategory.findPreference("key_about_lorem");
+            loremPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    DialogFragment dialog = new HelloWorldDialogFragment();
+                    dialog.show(getActivity().getFragmentManager(), "dialog_fragment_tag");
+                    return true;
+                }
+
+            });
+
+        }
+
     }
 
 }
