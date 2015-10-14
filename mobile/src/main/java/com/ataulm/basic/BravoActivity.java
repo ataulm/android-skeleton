5167786a79903bfd19f656b190378c64fd77e48f
Ataul Munim <timberwo7ves@gmail.com>
2015-10-14 12:00:10 +0100

    Add toast for key events

--- mobile/src/main/java/com/ataulm/basic/BravoActivity.java
+++ mobile/src/main/java/com/ataulm/basic/BravoActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +15,32 @@ import android.widget.TextView;
 import com.ataulm.rv.SpacesItemDecoration;
 
 public class BravoActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        Toaster.display(this, textRepresentationOfKeyCode(keyCode));
+        return super.onKeyDown(keyCode, event);
+    }
+
+    private static String textRepresentationOfKeyCode(int keyCode) {
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_DPAD_LEFT:
+                return "left";
+            case KeyEvent.KEYCODE_DPAD_UP:
+                return "up";
+            case KeyEvent.KEYCODE_DPAD_RIGHT:
+                return "right";
+            case KeyEvent.KEYCODE_DPAD_DOWN:
+                return "down";
+            case KeyEvent.KEYCODE_DPAD_CENTER:
+            case KeyEvent.KEYCODE_BUTTON_SELECT:
+            case KeyEvent.KEYCODE_ENTER:
+            case KeyEvent.KEYCODE_NUMPAD_ENTER:
+                return "select";
+            default:
+                return "key code: " + keyCode;
+        }
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
