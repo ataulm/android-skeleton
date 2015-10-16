c3fc87171127947ff1948ac46b6aa177566036a8
Ataul Munim <timberwo7ves@gmail.com>
2015-10-17 00:11:52 +0100

    Add click listener for toggling watch state

--- mobile/src/main/java/com/ataulm/basic/EpisodeNextUpViewHolder.java
+++ mobile/src/main/java/com/ataulm/basic/EpisodeNextUpViewHolder.java
@@ -1,39 +1,68 @@
 package com.ataulm.basic;
 
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 final class EpisodeNextUpViewHolder extends NextUpViewHolder {
 
     private final WatchStatusProvider watchStatusProvider;
     private final EpisodeClickListener clickListener;
+    private final TextView nameTextView;
+    private final ImageView watchedImageView;
 
     static EpisodeNextUpViewHolder inflate(ViewGroup parent, WatchStatusProvider watchStatusProvider, EpisodeClickListener clickListener) {
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
         View view = inflater.inflate(R.layout.view_next_up_item_episode, parent, false);
-        return new EpisodeNextUpViewHolder(view, watchStatusProvider, clickListener);
+        TextView nameTextView = (TextView) view.findViewById(R.id.next_up_episode_text_name);
+        ImageView watchedImageView = (ImageView) view.findViewById(R.id.next_up_episode_image_watched);
+        return new EpisodeNextUpViewHolder(view, watchStatusProvider, clickListener, nameTextView, watchedImageView);
     }
 
-    private EpisodeNextUpViewHolder(View itemView, WatchStatusProvider watchStatusProvider, EpisodeClickListener clickListener) {
+    private EpisodeNextUpViewHolder(View itemView, WatchStatusProvider watchStatusProvider, EpisodeClickListener clickListener, TextView nameTextView, ImageView watchedImageView) {
         super(itemView);
         this.watchStatusProvider = watchStatusProvider;
         this.clickListener = clickListener;
+        this.nameTextView = nameTextView;
+        this.watchedImageView = watchedImageView;
     }
 
     @Override
     public void bind(NextUpItem item) {
-        final Episode episode = (Episode) item.get();
-        ((TextView) itemView).setText(episode.getName());
+        Episode episode = (Episode) item.get();
+        nameTextView.setText(episode.getName());
+        updateWatchedButtonResource(episode);
+        setWatchButtonClickListener(episode);
+        setItemViewClickListener(episode);
+    }
 
+    private void updateWatchedButtonResource(Episode episode) {
+        // TODO: replace with image resources
+        if (watchStatusProvider.isWatched(episode)) {
+            watchedImageView.setBackgroundColor(Color.BLACK);
+        } else {
+            watchedImageView.setBackgroundColor(Color.BLUE);
+        }
+    }
+
+    private void setWatchButtonClickListener(final Episode episode) {
+        watchedImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickListener.onClickToggleWatched(episode);
+            }
+        });
+    }
+
+    private void setItemViewClickListener(final Episode episode) {
         itemView.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View v) {
                 clickListener.onClick(episode);
             }
-
         });
     }
 
