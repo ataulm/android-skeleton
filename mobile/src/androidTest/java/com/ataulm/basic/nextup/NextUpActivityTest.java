64c6d00e17fbe425325a6c5bc11bdc8d7e13420f
Ataul Munim <timberwo7ves@gmail.com>
2015-10-17 13:50:26 +0100

    Add UI test to assert episode watched button bg changes on click

--- mobile/src/androidTest/java/com/ataulm/basic/nextup/NextUpActivityTest.java
+++ mobile/src/androidTest/java/com/ataulm/basic/nextup/NextUpActivityTest.java
@@ -1,10 +1,22 @@
 package com.ataulm.basic.nextup;
 
+import android.graphics.drawable.Drawable;
+import android.support.test.espresso.NoMatchingViewException;
+import android.support.test.espresso.ViewAction;
+import android.support.test.espresso.ViewAssertion;
+import android.support.test.espresso.action.CoordinatesProvider;
+import android.support.test.espresso.action.GeneralClickAction;
+import android.support.test.espresso.action.Press;
+import android.support.test.espresso.action.Tap;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
+import android.widget.TextView;
 
 import com.ataulm.basic.R;
 
+import org.hamcrest.Description;
+import org.hamcrest.TypeSafeMatcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,6 +25,7 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.fest.assertions.api.Assertions.assertThat;
 
 @RunWith(AndroidJUnit4.class)
 public class NextUpActivityTest {
@@ -35,6 +48,85 @@ public class NextUpActivityTest {
     public void nextUpListIsDisplayed() {
         onView(withId(R.id.next_up_list))
                 .check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void watchedButtonBackgroundChangesOnClick() {
+        onView(isEpisodeViewWithName("New Captain"))
+                .check(EpisodeWatchedButtonDrawableChangedAssertion.cacheDrawablePreAction())
+                .perform(clickEpisodeWatchedButton())
+                .check(EpisodeWatchedButtonDrawableChangedAssertion.assertDrawableChangedPostAction());
+    }
+
+    private static TypeSafeMatcher<View> isEpisodeViewWithName(final String name) {
+        return new TypeSafeMatcher<View>() {
+
+            @Override
+            public boolean matchesSafely(View view) {
+                return isEpisodeView(view) && hasCorrectEpisodeName(view);
+            }
+
+            private boolean isEpisodeView(View view) {
+                View titlecard = view.findViewById(R.id.next_up_episode_image_titlecard);
+                return titlecard != null && titlecard.getParent() == view;
+            }
+
+            private boolean hasCorrectEpisodeName(View view) {
+                TextView nameTextView = (TextView) view.findViewById(R.id.next_up_episode_text_name);
+                return nameTextView.getText().equals(name);
+            }
+
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("NextUp Episode item with name: " + name);
+            }
+        };
+    }
+
+    private static class EpisodeWatchedButtonDrawableChangedAssertion {
+        private static Drawable drawable;
+
+        private static boolean started;
+
+        public static ViewAssertion cacheDrawablePreAction() {
+            return new ViewAssertion() {
+                @Override
+                public void check(View view, NoMatchingViewException e) {
+                    started = true;
+                    drawable = null;
+                    View button = view.findViewById(R.id.next_up_episode_image_watched);
+                    drawable = button.getBackground();
+                }
+            };
+        }
+
+        public static ViewAssertion assertDrawableChangedPostAction() {
+            return new ViewAssertion() {
+                @Override
+                public void check(View view, NoMatchingViewException e) {
+                    if (!started) {
+                        throw new IllegalStateException("cacheDrawablePreAction must be called before, else how can you compare?");
+                    }
+                    View button = view.findViewById(R.id.next_up_episode_image_watched);
+                    assertThat(drawable != button.getBackground());
+                }
+            };
+        }
+
+    }
+
+    private static ViewAction clickEpisodeWatchedButton() {
+        CoordinatesProvider coordinatesProvider = new CoordinatesProvider() {
+            @Override
+            public float[] calculateCoordinates(View view) {
+                View button = view.findViewById(R.id.next_up_episode_image_watched);
+                int[] xy = new int[2];
+                button.getLocationOnScreen(xy);
+
+                return new float[]{xy[0] + button.getWidth() / 2, xy[1] + button.getHeight() / 2};
+            }
+        };
+        return new GeneralClickAction(Tap.SINGLE, coordinatesProvider, Press.FINGER);
     }
 
 }
