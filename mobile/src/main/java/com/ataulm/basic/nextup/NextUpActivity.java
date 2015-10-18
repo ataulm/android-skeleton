1da6c13c12c5b80155e69de353f77ed67d163cdd
Ataul Munim <timberwo7ves@gmail.com>
2015-10-18 01:12:39 +0100

    Add customisation for LinearLayoutManager to support non-focusable views
    - this will allow dpad users to still be able to read section headers
    even though they aren't focusable
    - makes the assumption that there will never be two non-interactive views
    next to each other (the +1 / -1)
    - TODO: section headers should still be focusable if talkback is enabled

--- mobile/src/main/java/com/ataulm/basic/nextup/NextUpActivity.java
+++ mobile/src/main/java/com/ataulm/basic/nextup/NextUpActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.KeyEvent;
+import android.view.View;
 
 import com.ataulm.basic.R;
 import com.ataulm.basic.Toaster;
@@ -21,7 +23,32 @@ public class NextUpActivity extends Activity implements EpisodeClickListener {
         watchedRepository = WatchedRepository.newInstance(this);
 
         RecyclerView nextUpRecyclerView = ((RecyclerView) findViewById(R.id.next_up_list));
-        nextUpRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        nextUpRecyclerView.setLayoutManager(new LinearLayoutManager(this) {
+
+            @Override
+            public void addView(View child) {
+                super.addView(child);
+                child.setOnKeyListener(new View.OnKeyListener() {
+
+                    @Override
+                    public boolean onKey(View view, int keyCode, KeyEvent event) {
+                        if (event.getAction() != KeyEvent.ACTION_DOWN) {
+                            return false;
+                        }
+
+                        if (keyCode == KeyEvent.KEYCODE_DPAD_UP) {
+                            scrollToPosition(getPosition(view) - 1);
+                        } else if (keyCode == KeyEvent.KEYCODE_DPAD_DOWN) {
+                            scrollToPosition(getPosition(view) + 1);
+                        }
+                        return false;
+                    }
+
+                });
+            }
+
+        });
+
         adapter = new NextUpAdapter(NextUp.MOCK_DATA, watchedRepository, this);
         nextUpRecyclerView.setAdapter(adapter);
     }
