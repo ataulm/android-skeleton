5192ca9166744220bc19b9c18b4fbf36fb87a04a
Ataul Munim <timberwo7ves@gmail.com>
2015-10-18 11:16:21 +0100

    Set next up section headers as focusable when talkback enabled
    - didn't add focus state here because the TalkBack border will suffice
    - when talkback is disabled, this isn't focusable so no need for state

--- mobile/src/main/java/com/ataulm/basic/nextup/HeaderNextUpViewHolder.java
+++ mobile/src/main/java/com/ataulm/basic/nextup/HeaderNextUpViewHolder.java
@@ -5,24 +5,35 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.ataulm.basic.AccessibilityChecker;
 import com.ataulm.basic.R;
 
 final class HeaderNextUpViewHolder extends NextUpViewHolder {
 
+    private final AccessibilityChecker accessibilityChecker;
+
     static HeaderNextUpViewHolder inflate(ViewGroup parent) {
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
         View view = inflater.inflate(R.layout.view_next_up_item_header, parent, false);
-        return new HeaderNextUpViewHolder(view);
+        AccessibilityChecker accessibilityChecker = AccessibilityChecker.newInstance(parent.getContext());
+        return new HeaderNextUpViewHolder(view, accessibilityChecker);
     }
 
-    private HeaderNextUpViewHolder(View itemView) {
+    private HeaderNextUpViewHolder(View itemView, AccessibilityChecker accessibilityChecker) {
         super(itemView);
+        this.accessibilityChecker = accessibilityChecker;
     }
 
     @Override
     public void bind(NextUpItem item) {
         String header = (String) item.get();
         ((TextView) itemView).setText(header);
+
+        if (accessibilityChecker.isSpokenFeedbackEnabled()) {
+            itemView.setFocusable(true);
+        } else {
+            itemView.setFocusable(false);
+        }
     }
 
 }
