d2ad78cf58e1bcd7caa089d5da013e813f9b4b1a
Ataul Munim <timberwo7ves@gmail.com>
2015-10-24 21:57:42 +0100

    Remove buggy scroll assist

--- mobile/src/main/java/com/ataulm/basic/nextup/NextUpActivity.java
+++ mobile/src/main/java/com/ataulm/basic/nextup/NextUpActivity.java
@@ -5,11 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.KeyEvent;
+import android.util.Log;
 import android.view.Menu;
-import android.view.View;
 
-import com.ataulm.basic.KeyDownToaster;
 import com.ataulm.basic.R;
 import com.ataulm.basic.Toaster;
 
@@ -18,7 +16,6 @@ public class NextUpActivity extends AppCompatActivity implements EpisodeClickLis
     private WatchedRepository watchedRepository;
     private NextUpAdapter adapter;
     private RecyclerView nextUpRecyclerView;
-    private KeyDownToaster keyDownToaster;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -29,46 +26,10 @@ public class NextUpActivity extends AppCompatActivity implements EpisodeClickLis
         Toolbar toolbar = (Toolbar) findViewById(R.id.appbar_toolbar);
         setSupportActionBar(toolbar);
 
-        keyDownToaster = new KeyDownToaster(this);
-
         watchedRepository = WatchedRepository.newInstance(this);
 
         nextUpRecyclerView = ((RecyclerView) findViewById(R.id.next_up_list));
-        nextUpRecyclerView.setLayoutManager(new LinearLayoutManager(this) {
-
-            @Override
-            public void addView(View child) {
-                super.addView(child);
-                // This doesn't work so hot because often the view to which you want to scroll to isn't inflated
-                child.setOnKeyListener(new View.OnKeyListener() {
-
-                    @Override
-                    public boolean onKey(View view, int keyCode, KeyEvent event) {
-                        if (event.getAction() != KeyEvent.ACTION_DOWN) {
-                            return false;
-                        }
-
-                        if (keyCode == KeyEvent.KEYCODE_DPAD_UP || keyCode == KeyEvent.KEYCODE_NAVIGATE_PREVIOUS) {
-                            scrollToPosition(getPosition(view) - 2);
-                        } else if (keyCode == KeyEvent.KEYCODE_DPAD_DOWN || keyCode == KeyEvent.KEYCODE_NAVIGATE_NEXT || keyCode == KeyEvent.KEYCODE_TAB) {
-                            scrollToPosition(getPosition(view) + 2);
-                        }
-
-                        if (keyCode == KeyEvent.KEYCODE_TAB && getPosition(view) == getItemCount() - 1) {
-                            // TODO: maybe display dialog here instead:
-                            // "Focus on AppBar"
-                            // "Scroll to top of list"
-                            // "Cancel"
-                            scrollToPosition(0);
-                        }
-                        return false;
-                    }
-
-                });
-            }
-
-        });
-
+        nextUpRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         adapter = new NextUpAdapter(NextUp.MOCK_DATA, watchedRepository, this);
         nextUpRecyclerView.setAdapter(adapter);
     }
@@ -88,17 +49,13 @@ public class NextUpActivity extends AppCompatActivity implements EpisodeClickLis
     public void onClickToggleWatched(Episode episode) {
         watchedRepository.toggledWatched(episode);
         adapter.notifyDataSetChanged();
-
         if (watchedRepository.isWatched(episode)) {
+            Log.e("FOO", "marked " + episode.getName() + " as watched");
             nextUpRecyclerView.announceForAccessibility("Marked " + episode.getName() + " as watched.");
         } else {
+            Log.e("FOO", "marked " + episode.getName() + " as not watched");
             nextUpRecyclerView.announceForAccessibility("Marked " + episode.getName() + " as not watched.");
         }
-    }
-
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        return keyDownToaster.onKeyDown(keyCode, event);
     }
 
 }
