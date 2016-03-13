4cee0594ef2974838a67224fddba27238fefff80
Ataul Munim <hello@ataulm.com>
2016-03-13 21:28:57 +0000

    Add search action

--- mobile/src/main/java/com/ataulm/basic/MyActivity.java
+++ mobile/src/main/java/com/ataulm/basic/MyActivity.java
@@ -6,11 +6,16 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MyActivity extends AppCompatActivity {
+
+    private Toast toast;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -23,6 +28,29 @@ public class MyActivity extends AppCompatActivity {
         RecyclerView recyclerView = ((RecyclerView) findViewById(R.id.list));
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(new ItemAdapter());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.foo, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.search) {
+            display("search");
+            return true;
+        }
+        return false;
+    }
+
+    private void display(String message) {
+        if (toast != null) {
+            toast.cancel();
+        }
+        toast = Toast.makeText(this, message, Toast.LENGTH_SHORT);
+        toast.show();
     }
 
     private static class ItemAdapter extends RecyclerView.Adapter<ItemViewHolder> {
