ad9c3f2045873e8c8dbffaad9aed79242b83c595
Ataul Munim <hello@ataulm.com>
2016-03-13 20:55:13 +0000

    Add initial activity

--- mobile/src/main/java/com/ataulm/basic/MyActivity.java
+++ mobile/src/main/java/com/ataulm/basic/MyActivity.java
@@ -2,6 +2,13 @@ package com.ataulm.basic;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class MyActivity extends AppCompatActivity {
 
@@ -9,6 +16,38 @@ public class MyActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+        setTitle("What");
+
+        RecyclerView recyclerView = ((RecyclerView) findViewById(R.id.list));
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new ItemAdapter());
     }
+
+    private static class ItemAdapter extends RecyclerView.Adapter<ItemViewHolder> {
+        @Override
+        public ItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.view_item, parent, false);
+            return new ItemViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(ItemViewHolder holder, int position) {
+            ((TextView) holder.itemView).setText(String.valueOf(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return 50;
+        }
+    }
+
+    private static class ItemViewHolder extends RecyclerView.ViewHolder {
+        public ItemViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+
 
 }
