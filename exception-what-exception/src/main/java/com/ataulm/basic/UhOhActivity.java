8386f61025fb1d807108fa285b1ff3a697075e65
Ataul Munim <hello@ataulm.com>
2016-03-26 21:10:28 +0000

    Simplify uh oh screen

--- exception-what-exception/src/main/java/com/ataulm/basic/UhOhActivity.java
+++ exception-what-exception/src/main/java/com/ataulm/basic/UhOhActivity.java
@@ -2,8 +2,6 @@ package com.ataulm.basic;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.widget.TextView;
 
 public class UhOhActivity extends AppCompatActivity {
 
@@ -11,10 +9,6 @@ public class UhOhActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_uh_oh);
-        TextView view = (TextView) findViewById(R.id.uhoh_text);
-
-        view.setText("uh oh got an exception");
-        Log.e("RUHROH", "in ze uh oh activity");
     }
 
 }
