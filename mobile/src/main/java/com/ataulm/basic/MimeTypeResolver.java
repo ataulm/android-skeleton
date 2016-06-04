2c2c8cab9bcd84840c534a77687872342c10be21
Ataul Munim <hello@ataulm.com>
2016-06-04 01:34:24 +0100

    Add MimeTypeResolver

---
+++ mobile/src/main/java/com/ataulm/basic/MimeTypeResolver.java
@@ -0,0 +1,39 @@
+package com.ataulm.basic;
+
+import android.content.Context;
+import android.net.Uri;
+import android.support.annotation.Nullable;
+
+class MimeTypeResolver {
+
+    private static final String MIME_TYPE_TOP_LEVEL_ACTIVITY = "vnd.android.cursor.item/vnd.%1$s.toplevel";
+
+    private final String packageName;
+
+    static MimeTypeResolver create(Context context) {
+        return new MimeTypeResolver(context.getPackageName());
+    }
+
+    MimeTypeResolver(String packageName) {
+        this.packageName = packageName;
+    }
+
+    @Nullable
+    public String getType(Uri uri) {
+        if (validTopLevel(uri)) {
+            return String.format(MIME_TYPE_TOP_LEVEL_ACTIVITY, packageName);
+        } else {
+            return null;
+        }
+    }
+
+    private boolean validTopLevel(Uri uri) {
+        for (Screen screen : Screen.values()) {
+            if (uri.getLastPathSegment().endsWith(screen.getPath())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
