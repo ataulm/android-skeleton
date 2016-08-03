b984a15dd9205f1284fc873fbe1bf80da8353ee0
Ataul Munim <hello@ataulm.com>
2016-08-03 12:25:54 +0100

    Add Fabric

--- mobile/src/main/java/com/ataulm/basic/MyActivity.java
+++ mobile/src/main/java/com/ataulm/basic/MyActivity.java
@@ -2,13 +2,29 @@ package com.ataulm.basic;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.crashlytics.android.Crashlytics;
+import com.crashlytics.android.beta.Beta;
+
+import io.fabric.sdk.android.Fabric;
 
 public class MyActivity extends AppCompatActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics(), new Beta());
         setContentView(R.layout.activity_my);
+    }
+
+    public void onClickEventOne(View view) {
+    }
+
+    public void onClickEventTwo(View view) {
+    }
+
+    public void onClickEventThree(View view) {
     }
 
 }
