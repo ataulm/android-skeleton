ff4d46d4694dc892d08d1606c06a0c69544bd10c
Ryan Feline <ryan.feline@novoda.com>
2016-08-24 15:29:21 +0100

    temp II fullstop.

--- mobile/src/main/java/com/ataulm/basic/MyActivity.java
+++ mobile/src/main/java/com/ataulm/basic/MyActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.support.annotation.ColorInt;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
 
 public class MyActivity extends AppCompatActivity {
 
@@ -44,7 +46,14 @@ public class MyActivity extends AppCompatActivity {
     }
 
     public void showDialog(View view) {
-        final Dialog dialog = new Dialog(this, R.style.FullScreenDialog);
+        final Dialog dialog = new Dialog(this, R.style.FullScreenDialog) {
+            @Override
+            protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+                getWindow().clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
+            }
+        };
         SimpleDialogView inflate = (SimpleDialogView) getLayoutInflater().inflate(R.layout.view_simple_dialog, null);
         inflate.bind(new SimpleDialogView.Callback() {
             @Override
