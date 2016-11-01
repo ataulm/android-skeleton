d90adbcbbd035e693529af9c9830f1713761c905
Ataul Munim <hello@ataulm.com>
2016-11-01 10:48:18 +0000

    demo

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
-android-basic
-=============
-Can be used as a skeleton for new Android projects.
+This is to demonstrate how the enabled state of nested views each with click listeners can affect the visual state:
+
+Item view is our container
+Inner action is a textview inside
+
+Item view always has a click listener
+Inner action sometimes does.
+
+When inner action has NO click listener but is enabled,
+clicking on the item view will trigger a separate ripple on the inner action
+
+This is because viewgroups will by default propagate their pressed state to children        http://stackoverflow.com/a/6743126/494879
+
+To fix this, set the inner action to be disabled.
--- mobile/src/main/java/com/ataulm/basic/MyActivity.java
+++ mobile/src/main/java/com/ataulm/basic/MyActivity.java
@@ -2,13 +2,77 @@ package com.ataulm.basic;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MyActivity extends AppCompatActivity {
+
+    private View button;
+    private View innerAction;
+    private View itemview;
+    private Button enableButton;
+    private Button clickListenerButton;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+
+        itemview = findViewById(R.id.itemview);
+        innerAction = findViewById(R.id.inner_action);
+        enableButton = (Button) findViewById(R.id.enableButton);
+        clickListenerButton = (Button) findViewById(R.id.clickListenerButton);
+
+        final View.OnClickListener itemViewClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                log("itemview click");
+            }
+        };
+
+        final View.OnClickListener innerActionClickLIstener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                log("innerAction click");
+            }
+        };
+
+        itemview.setOnClickListener(itemViewClickListener);
+        innerAction.setOnClickListener(innerActionClickLIstener);
+
+        clickListenerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (innerAction.hasOnClickListeners()) {
+                    innerAction.setOnClickListener(null);
+                    innerAction.setClickable(false);
+                } else {
+                    innerAction.setOnClickListener(innerActionClickLIstener);
+                }
+
+                updateLabels();
+            }
+        });
+
+        enableButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                itemview.setEnabled(!innerAction.isEnabled());
+                updateLabels();
+            }
+        });
+
+        updateLabels();
+    }
+
+    private void updateLabels() {
+        enableButton.setText("is enabled: " + innerAction.isEnabled());
+        clickListenerButton.setText("has listener: " + innerAction.hasOnClickListeners());
+    }
+
+    private void log(String message) {
+        Log.i("!!!", message);
     }
 
 }
