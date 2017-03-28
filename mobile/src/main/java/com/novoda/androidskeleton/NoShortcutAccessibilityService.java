2c2af973fb783e02700a0d18516fff9293bd79c8
Ataul Munim <hello@ataulm.com>
2017-03-28 07:09:53 +0100

    Remove everything except a11y service

---
+++ mobile/src/main/java/com/novoda/androidskeleton/NoShortcutAccessibilityService.java
@@ -0,0 +1,10 @@
+package com.novoda.androidskeleton;
+
+import android.accessibilityservice.AccessibilityService;
+
+public class NoShortcutAccessibilityService extends AccessibilityService {
+
+    // note we are not specifying any flags
+    // we are especially not specifying AccessibilityServiceInfo.FLAG_REQUEST_ACCESSIBILITY_BUTTON
+
+}
