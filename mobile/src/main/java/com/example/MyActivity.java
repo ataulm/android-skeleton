04b49a56f8a9b004fd3057064a6e780d6e838bfb
Ataul Munim <hello@ataulm.com>
2017-10-13 17:04:46 +0100

    Add logs for moving app to foreground/background

--- mobile/src/main/java/com/example/MyActivity.java
+++ mobile/src/main/java/com/example/MyActivity.java
@@ -1,7 +1,14 @@
 package com.example;
 
+import android.arch.lifecycle.Lifecycle;
+import android.arch.lifecycle.LifecycleObserver;
+import android.arch.lifecycle.OnLifecycleEvent;
+import android.arch.lifecycle.ProcessLifecycleOwner;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 
 public class MyActivity extends AppCompatActivity {
 
@@ -9,5 +16,25 @@ public class MyActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+
+        Lifecycle lifecycle = ProcessLifecycleOwner.get().getLifecycle();
+        lifecycle.addObserver(new LifecycleObserver() {
+
+            @OnLifecycleEvent(Lifecycle.Event.ON_START)
+            public void onApplicationMovingToForeground() {
+                Log.d("!!! ", "onApplicationMovingToForeground");
+            }
+
+            @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+            public void onApplicationMovingToBackground() {
+                Log.d("!!! ", "onApplicationMovingToBackground");
+            }
+
+        });
+    }
+
+    public void openSecondActivity(View view) {
+        Intent intent = new Intent(this, SecondActivityInApp.class);
+        startActivity(intent);
     }
 }
---
+++ mobile/src/main/java/com/example/SecondActivityInApp.java
@@ -0,0 +1,6 @@
+package com.example;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class SecondActivityInApp extends AppCompatActivity {
+}
