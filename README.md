7c4f1931c562287ac80faeb33d127c69f317a96e
Ataul Munim <hello@ataulm.com>
2018-03-18 16:29:35 +0000

    add skele for bug repro

--- README.md
+++ README.md
@@ -1,11 +1,29 @@
 android-example
 ===============
-Can be used as a skeleton for new spikes. Don't modify this project except to update dependencies; checkout from the latest master.
 
-It would be good to fill this out before starting the spike and with updates before each push if necessary:
+Does anyone have any clues as to when `View.requestAccessibilityFocus` would be called on a view, by the system? e.g. in response to some event.
 
-#### Aim
-_e.g. wanted to try to create a completely virtual view hierarchy, so there's nothing really there on screen but an accessibility service like TalkBack could navigate it and a user proficient with TalkBack could use the app_
+We have a button which, when tapped, animates an off-screen view _onto_ the screen, like an overlay. This overlay contains a RecyclerView of RecyclerViews, and we're finding that TalkBack is focusing on the first item in the first recyclerview *after some time* (so not immediately).
 
-#### Current state (inc. next steps if applicable)
-_e.g. completed - see blahblah.kt for main bits._
+Prior to the RV is a toolbar, so it's not like it's focusing on the first child in the overlay either :thinking_face: (edited)
+
+:grin: it looks like it's happening when the clock rolls over to the next minute :facepalm:
+
+1) not sure why it's doing that in the first place - maybe because the window content has changed but there's nothing in focus currently?
+2) not sure why it's choosing that view to do it
+
+testing on an oreo emulator. (edited)
+
+Ataul Munim [10:47 AM]
+For a "workaround" :face_vomiting: , I'm manually requesting focus on the toolbar title - it seems if there's something focused already, it doesn't try to "find focus" on something else when the time changes.
+
+this is not ideal because:
+
+- there's a dismiss button on the left of the toolbar title
+- it might break with future versions of talkback
+- unsure if the find focus thing is a bug which will be fixed so I'm left with a patch to a non-issue
+
+---
+
+state: couldn't yet repro the issue. on this branch, it doesn't care that nothing is focused when the clock ticks over to the next minute.
+I wonder if the fact we had a video playing affected it.
--- app/src/main/java/com/example/MyActivity.kt
+++ app/src/main/java/com/example/MyActivity.kt
@@ -2,11 +2,111 @@ package com.example
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.view.doOnLayout
+import kotlinx.android.synthetic.main.activity_my.*
 
 class MyActivity : AppCompatActivity() {
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my)
+
+        overlay.doOnLayout { translateOverlayOffScreen() }
+        open_overlay_button.setOnClickListener({
+            overlay.translationX = 0.toFloat()
+            open_overlay_button.importantForAccessibility = View.IMPORTANT_FOR_ACCESSIBILITY_NO
+        })
+        close_overlay_button.setOnClickListener({
+            translateOverlayOffScreen()
+            open_overlay_button.importantForAccessibility = View.IMPORTANT_FOR_ACCESSIBILITY_AUTO
+        })
+
+        vertical_list.layoutManager = LinearLayoutManager(this)
+        vertical_list.adapter = VerticalListAdapter()
+    }
+
+    private fun translateOverlayOffScreen() {
+        overlay.translationX = overlay.width.toFloat()
+    }
+
+    private class VerticalListAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            return HorizontalListViewHolder.inflateHorizontalListView(parent)
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            val row = holder.itemView as RecyclerView
+            row.layoutManager = LinearLayoutManager(holder.itemView.context, RecyclerView.HORIZONTAL, false)
+            row.adapter = RowAdapter()
+        }
+
+        override fun getItemCount(): Int {
+            return 3
+        }
+    }
+
+    private class HorizontalListViewHolder(itemView: RecyclerView) : RecyclerView.ViewHolder(itemView) {
+
+        companion object {
+            fun inflateHorizontalListView(parent: ViewGroup): HorizontalListViewHolder {
+                val view = LayoutInflater.from(parent.context).inflate(R.layout.item_horizontal_list, parent, false)
+                return HorizontalListViewHolder(view as RecyclerView)
+            }
+        }
+    }
+
+    private class RowAdapter : RecyclerView.Adapter<SingleItemViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SingleItemViewHolder {
+            return SingleItemViewHolder.inflateSingleItemView(parent)
+        }
+
+        override fun onBindViewHolder(holder: SingleItemViewHolder, position: Int) {
+            (holder.itemView as TextView).text = "item $position"
+            holder.itemView.setOnClickListener({ Toast.makeText(holder.itemView.context, "click $position", Toast.LENGTH_SHORT).show()})
+        }
+
+        override fun getItemCount(): Int {
+            return 5
+        }
+    }
+
+    private class SingleItemViewHolder(itemView: TextView) : RecyclerView.ViewHolder(itemView) {
+
+        companion object {
+            fun inflateSingleItemView(parent: ViewGroup): SingleItemViewHolder {
+                val view = LayoutInflater.from(parent.context).inflate(R.layout.item_single, parent, false)
+                return SingleItemViewHolder(view as TextView)
+            }
+        }
+    }
+}
+
+fun View.recursivelySetNotImportant() {
+    setTag(R.id.important_for_accessibility, importantForAccessibility)
+    importantForAccessibility = View.IMPORTANT_FOR_ACCESSIBILITY_NO
+    if (this is ViewGroup) {
+        val viewGroup = this
+        for (i in 0 until viewGroup.childCount) {
+            viewGroup.getChildAt(i).recursivelySetNotImportant()
+        }
+    }
+}
+
+fun View.recursivelyResetImportanceOrAuto() {
+    val tag = getTag(R.id.important_for_accessibility)
+    importantForAccessibility = if (tag != null) tag as Int else View.IMPORTANT_FOR_ACCESSIBILITY_AUTO
+
+    if (this is ViewGroup) {
+        val viewGroup = this
+        for (i in 0 until viewGroup.childCount) {
+            viewGroup.getChildAt(i).recursivelyResetImportanceOrAuto()
+        }
     }
 }
