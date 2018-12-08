1ab1b54268a655ccbbd210f4bfaf2bef1e65ff70
Ataul Munim <hello@ataulm.com>
2018-12-08 17:40:16 +0000

    Add swipe to delete

--- app/src/main/java/com/example/ItemsAdapter.kt
+++ app/src/main/java/com/example/ItemsAdapter.kt
@@ -4,25 +4,45 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.view_number.view.*
 
-class ItemsAdapter(val onClick: (Int) -> Unit) : ListAdapter<Int, NumberViewHolder>(Differ) {
+class ItemsAdapter(val onClick: (String) -> Unit) : ListAdapter<String, NumberViewHolder>(Differ) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = NumberViewHolder.inflate(parent)
 
     override fun onBindViewHolder(holder: NumberViewHolder, position: Int) {
         val item = getItem(position)
-        holder.itemView.numberTextView.text = "$item number!"
+        holder.itemView.numberTextView.text = item
         holder.itemView.setOnClickListener { onClick(item) }
     }
 
-    object Differ : DiffUtil.ItemCallback<Int>() {
-        override fun areItemsTheSame(oldItem: Int, newItem: Int): Boolean = oldItem == newItem
-        override fun areContentsTheSame(oldItem: Int, newItem: Int): Boolean = oldItem == newItem
+    object Differ : DiffUtil.ItemCallback<String>() {
+        override fun areItemsTheSame(oldItem: String, newItem: String): Boolean = oldItem == newItem
+        override fun areContentsTheSame(oldItem: String, newItem: String): Boolean = oldItem == newItem
     }
 }
+
+class SwipeToDeleteCallback(private val onSwipeToDeleteAtAdapterPosition: (Int) -> Unit) : ItemTouchHelper.Callback() {
+
+    override fun getMovementFlags(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder): Int {
+        return ItemTouchHelper.Callback.makeMovementFlags(NO_DIRECTIONS, ItemTouchHelper.END)
+    }
+
+    override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+        onSwipeToDeleteAtAdapterPosition(viewHolder.adapterPosition)
+    }
+
+    override fun onMove(rv: RecyclerView, vh: RecyclerView.ViewHolder, t: RecyclerView.ViewHolder) = false
+
+    companion object {
+
+        private const val NO_DIRECTIONS = 0
+    }
+}
+
 
 class NumberViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
--- app/src/main/java/com/example/MyActivity.kt
+++ app/src/main/java/com/example/MyActivity.kt
@@ -3,23 +3,31 @@ package com.example
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.ItemTouchHelper
 import kotlinx.android.synthetic.main.activity_my.*
 
 class MyActivity : AppCompatActivity() {
 
     private val itemsAdapter = ItemsAdapter { toast(it) }
-    private val numberList = IntArray(100) { it }.toList()
+    private val list = IntArray(100) { it }.map { "item: $it" }.toMutableList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my)
         recyclerView.adapter = itemsAdapter
 
-        itemsAdapter.submitList(numberList)
+        itemsAdapter.submitList(list.toList())
+
+        ItemTouchHelper(
+                SwipeToDeleteCallback { position ->
+                    list.removeAt(position)
+                    itemsAdapter.submitList(list.toList())
+                }
+        ).attachToRecyclerView(recyclerView)
     }
 
     private var toast: Toast? = null
-    private fun toast(value: Int) {
+    private fun toast(value: String) {
         toast?.cancel()
         toast = Toast.makeText(this, "click $value", Toast.LENGTH_SHORT).apply { show() }
     }
