2af300e4a1e192a3270d786d61b324e27ea00d7a
Ataul Munim <hello@ataulm.com>
2018-12-08 16:55:39 +0000

    Extract ItemsAdapter to another file

--- app/src/main/java/com/example/MyActivity.kt
+++ app/src/main/java/com/example/MyActivity.kt
@@ -1,16 +1,9 @@
 package com.example
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.ListAdapter
-import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_my.*
-import kotlinx.android.synthetic.main.view_number.view.*
 
 class MyActivity : AppCompatActivity() {
 
@@ -32,29 +25,3 @@ class MyActivity : AppCompatActivity() {
     }
 }
 
-private class ItemsAdapter(val onClick: (Int) -> Unit) : ListAdapter<Int, NumberViewHolder>(Differ) {
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = NumberViewHolder.inflate(parent)
-
-    override fun onBindViewHolder(holder: NumberViewHolder, position: Int) {
-        val item = getItem(position)
-        holder.itemView.numberTextView.text = "$item number!"
-        holder.itemView.setOnClickListener { onClick(item) }
-    }
-
-    object Differ : DiffUtil.ItemCallback<Int>() {
-        override fun areItemsTheSame(oldItem: Int, newItem: Int): Boolean = oldItem == newItem
-        override fun areContentsTheSame(oldItem: Int, newItem: Int): Boolean = oldItem == newItem
-    }
-}
-
-private class NumberViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-
-    companion object {
-
-        fun inflate(parent: ViewGroup): NumberViewHolder {
-            val view = LayoutInflater.from(parent.context).inflate(R.layout.view_number, parent, false)
-            return NumberViewHolder(view)
-        }
-    }
-}
