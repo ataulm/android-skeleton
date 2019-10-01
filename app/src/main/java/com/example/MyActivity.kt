3a7890fe5c573e12a5ad0e88bc076e59ca0c83d9
Ataul Munim <hello@ataulm.com>
2019-10-01 22:14:56 +0100

    Add basic playground

--- app/src/main/java/com/example/MyActivity.kt
+++ app/src/main/java/com/example/MyActivity.kt
@@ -1,12 +1,24 @@
 package com.example
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_my.*
 
 class MyActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my)
+
+        darkButton.setOnClickListener {
+           startActivity(Intent(this, DarkThemeActivity::class.java))
+        }
+        lightButton.setOnClickListener {
+           startActivity(Intent(this, LightThemeActivity::class.java))
+        }
     }
 }
+
+class DarkThemeActivity : AppCompatActivity(R.layout.activity_material_design_showcase)
+class LightThemeActivity : AppCompatActivity(R.layout.activity_material_design_showcase)
