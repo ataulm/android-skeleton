e93efd82d01167bd3159e8ae7702aad6fa6ec1f6
Ataul Munim <hello@ataulm.com>
2019-11-19 22:37:03 +1100

    Add fragment demo

--- README.md
+++ README.md
@@ -1,11 +1,7 @@
-android-example
-===============
-Can be used as a skeleton for new spikes. Don't modify this project except to update dependencies; checkout from the latest master.
+highlights how fragments that aren't on top are still focusable by talkback.
 
-It would be good to fill this out before starting the spike and with updates before each push if necessary:
+expected behaviour:
+- iterating through UI elements should include only "Button B"
 
-#### Aim
-_e.g. wanted to try to create a completely virtual view hierarchy, so there's nothing really there on screen but an accessibility service like TalkBack could navigate it and a user proficient with TalkBack could use the app_
-
-#### Current state (inc. next steps if applicable)
-_e.g. completed - see blahblah.kt for main bits._
+actual:
+- starts with "Button A", then "Button B", even though A is not visible
--- app/src/main/java/com/example/MyActivity.kt
+++ app/src/main/java/com/example/MyActivity.kt
@@ -1,12 +1,30 @@
 package com.example
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 
 class MyActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_my)
+        supportFragmentManager.beginTransaction()
+                .add(android.R.id.content, ButtonedFragment(R.layout.fragment_buttoned_one), "a")
+                .commit()
+
+        supportFragmentManager.beginTransaction()
+                .add(android.R.id.content, ButtonedFragment(R.layout.fragment_buttoned_two), "b")
+                .addToBackStack(null)
+                .commit()
+    }
+}
+
+internal class ButtonedFragment(private val layoutId: Int) : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(layoutId, container, false)
     }
 }
