475f0b389bf682decf97a591a98d037242e559b6
Ataul Munim <hello@ataulm.com>
2020-02-13 12:47:29 +0000

    Add compose-friendly activity

---
+++ app/src/main/java/com/example/MyComposeActivity.kt
@@ -0,0 +1,11 @@
+package com.example
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MyComposeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
