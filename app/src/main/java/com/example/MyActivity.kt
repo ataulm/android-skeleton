cf9c6e2e569ef3ac216cd20f2dc39ab522d381ad
Ataul Munim <hello@ataulm.com>
2020-02-15 16:13:36 +0000

    Update manifest

--- app/src/main/java/com/example/MyActivity.kt
+++ app/src/main/java/com/example/MyActivity.kt
@@ -2,11 +2,13 @@ package com.example
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
 
 class MyActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my)
+
     }
 }
