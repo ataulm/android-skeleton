145c6ce5a3cf702d47364a4965b5265aec59a646
Ataul Munim <hello@ataulm.com>
2020-02-15 16:39:53 +0000

    Add examples

--- app/src/main/java/com/example/MyActivity.kt
+++ app/src/main/java/com/example/MyActivity.kt
@@ -1,8 +1,13 @@
 package com.example
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.AccessibilityDelegateCompat
 import androidx.core.view.ViewCompat
+import androidx.core.view.accessibility.AccessibilityNodeInfoCompat
+import androidx.core.view.accessibility.AccessibilityViewCommand
+import kotlinx.android.synthetic.main.activity_my.*
 
 class MyActivity : AppCompatActivity() {
 
@@ -10,5 +15,28 @@ class MyActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my)
 
+        ViewCompat.setAccessibilityHeading(primaryButton, true)
+        ViewCompat.addAccessibilityAction(primaryButton, "custom action") { view: View, commandArguments: AccessibilityViewCommand.CommandArguments? ->
+            // TODO do something
+            true
+        }
+        ViewCompat.setAccessibilityDelegate(primaryButton, object : AccessibilityDelegateCompat() {
+            override fun onInitializeAccessibilityNodeInfo(host: View?, info: AccessibilityNodeInfoCompat?) {
+                super.onInitializeAccessibilityNodeInfo(host, info)
+                info?.addAction( AccessibilityNodeInfoCompat.AccessibilityActionCompat(
+                        R.id.fooo, "custom action2"
+                ) )
+            }
+        })
+
+        // this one overrides the previous accessibility delegate such that only `custom action` and `custom action3` are available (isHeading is also preserved)
+        ViewCompat.setAccessibilityDelegate(primaryButton, object : AccessibilityDelegateCompat() {
+            override fun onInitializeAccessibilityNodeInfo(host: View?, info: AccessibilityNodeInfoCompat?) {
+                super.onInitializeAccessibilityNodeInfo(host, info)
+                info?.addAction( AccessibilityNodeInfoCompat.AccessibilityActionCompat(
+                        R.id.bar, "custom action3"
+                ) )
+            }
+        })
     }
 }
