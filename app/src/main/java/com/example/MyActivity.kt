ffb0104fe76263865b96143798f74fef7dc84c36
Ataul Munim <hello@ataulm.com>
2020-03-18 15:34:38 +0000

    Add basic list pref setup

--- app/src/main/java/com/example/MyActivity.kt
+++ app/src/main/java/com/example/MyActivity.kt
@@ -2,13 +2,22 @@ package com.example
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
+import androidx.preference.PreferenceFragmentCompat
 
 class MyActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_my)
-
+        // Display the fragment as the main content
+        supportFragmentManager.beginTransaction()
+                .replace(android.R.id.content, SettingsFragment()).commit()
     }
 }
+
+class SettingsFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preference_screen, rootKey)
+    }
+}
+
+
