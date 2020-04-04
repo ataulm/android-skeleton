a3314e23f40b05f38f3a1c2b3a656ab75e300331
Ataul Munim <hello@ataulm.com>
2020-04-04 19:03:58 +0100

    Add FramedPictureView

--- app/src/main/java/com/example/MyActivity.kt
+++ app/src/main/java/com/example/MyActivity.kt
@@ -1,6 +1,11 @@
 package com.example
 
+import android.content.Context
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+import android.widget.FrameLayout
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 
@@ -10,5 +15,17 @@ class MyActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my)
 
+    }
+}
+
+class FramedPictureView(context: Context, attrs: AttributeSet) : LinearLayout(context, attrs) {
+
+    init {
+        super.setOrientation(VERTICAL)
+    }
+
+    override fun onFinishInflate() {
+        super.onFinishInflate()
+        View.inflate(context, R.layout.merge_framed_picture, this)
     }
 }
