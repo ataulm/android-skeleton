355629bcc799f50ee2a9ef5299fc51b13127bebf
Ataul Munim <hello@ataulm.com>
2020-06-19 13:43:28 +0100

    Rename main activity

--- app/src/main/java/com/example/presentation/PupzListActivity.kt
+++ app/src/main/java/com/example/presentation/PupzListActivity.kt
@@ -1,13 +1,13 @@
-package com.example
+package com.example.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.R
 
-class MyActivity : AppCompatActivity() {
+class PupzListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_my)
-
+        setContentView(R.layout.activity_pupz_list)
     }
 }
