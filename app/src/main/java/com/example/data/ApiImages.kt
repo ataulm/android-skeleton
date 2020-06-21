2c4cb582f5a6c60c25837f131027861d6bcc8a1e
Ataul Munim <hello@ataulm.com>
2020-06-21 18:18:45 +0100

    Add signing configs to facilitate sharing release apk

--- app/src/main/java/com/example/data/ApiImages.kt
+++ app/src/main/java/com/example/data/ApiImages.kt
@@ -1,7 +1,9 @@
 package com.example.data
 
 import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
 
+@JsonClass(generateAdapter = true)
 internal data class ApiImages(
         @Json(name = "message") val message: List<String>,
         @Json(name = "status") val status: String
--- app/src/main/java/com/example/data/ApiResponse.kt
+++ app/src/main/java/com/example/data/ApiResponse.kt
@@ -1,7 +1,9 @@
 package com.example.data
 
 import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
 
+@JsonClass(generateAdapter = true)
 internal data class ApiResponse(
         @Json(name = "message") val message: Map<String, List<String>>,
         @Json(name = "status") val status: String
