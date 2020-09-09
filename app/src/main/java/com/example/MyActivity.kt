ce7d42c39e45eaf0f40e9599d784b482440230be
Ataul Munim <hello@ataulm.com>
2020-09-09 12:49:11 +0100

    Add column with image and text
    - [Sections 1 and 2](https://developer.android.com/jetpack/compose/tutorial)

--- app/src/main/java/com/example/MyActivity.kt
+++ app/src/main/java/com/example/MyActivity.kt
@@ -2,13 +2,40 @@ package com.example
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.imageFromResource
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.unit.dp
+import androidx.ui.tooling.preview.Preview
 
 class MyActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_my)
-
+        setContent { NewsStory() }
     }
+
+    @Composable
+    fun NewsStory() {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Image(
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier.fillMaxWidth(),
+                    asset = imageFromResource(resources, R.drawable.header)
+            )
+            Spacer(modifier = Modifier.preferredHeight(16.dp))
+            Text(text = "A day in Shark Fin cove")
+            Text(text = "Davenport California")
+            Text(text = "December 2018")
+        }
+    }
+
+    @Preview
+    @Composable
+    fun PreviewNewsStory() = NewsStory()
 }
