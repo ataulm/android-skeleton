f1b4947551d70b2c539408fffbae7dd61d827937
Ataul Munim <hello@ataulm.com>
2021-02-06 13:57:56 +0000

    Update dependencies and then do nothing for the rest of the day

--- app/src/main/java/com/example/MyActivity.kt
+++ app/src/main/java/com/example/MyActivity.kt
@@ -2,13 +2,11 @@ package com.example
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
 
 class MyActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my)
-
     }
 }
