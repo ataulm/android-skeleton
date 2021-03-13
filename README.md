fcf56e821141bdfd9ee26d4e6cbbe2c1706ade9c
Ataul Munim <hello@ataulm.com>
2021-03-13 19:37:00 +0000

    Add popupwindow examples

--- README.md
+++ README.md
@@ -5,7 +5,16 @@ Can be used as a skeleton for new spikes. Don't modify this project except to up
 It would be good to fill this out before starting the spike and with updates before each push if necessary:
 
 #### Aim
-_e.g. wanted to try to create a completely virtual view hierarchy, so there's nothing really there on screen but an accessibility service like TalkBack could navigate it and a user proficient with TalkBack could use the app_
+Create and position PopupWindow.
 
 #### Current state (inc. next steps if applicable)
-_e.g. completed - see blahblah.kt for main bits._
+Using `Gravity.NO_GRAVITY` seems to be the least confusing (vs. switching it based on the case). That way it's offset from the top left of the screen.
+
+The thing that's still unclear is that using an offset for Y that's 0-24 seems to be ignored (that's the size of a statusbar...). I wonder if it's clamping the offset to `min(statusbar_height, value)`!
+
+Similarly when using `Gravity.BOTTOM`, it would seem to clamp for the navigation bar height. This is pretty frustrating because it means that on a rotated device (where the nav bar might be on the side) the clamp doesn't apply, so if you used a value that's less than the nav bar height, it'll be ignored in portrait and visible in landscape 😠
+
+This is why it makes sense to use `NO_GRAVITY` - this is equivalent to `TOP or LEFT` and since the status bar is likely to be unaffected by orientation, it seems like the best way to go.
+
+Positioning in the x-axis needs to be done carefully, because of RTL mode. Lots of conditionals.
+
--- app/src/main/java/com/example/MyActivity.kt
+++ app/src/main/java/com/example/MyActivity.kt
@@ -1,12 +1,142 @@
 package com.example
 
+import android.content.res.Resources
+import android.graphics.Point
 import android.os.Bundle
+import android.util.Size
+import android.view.Gravity
+import android.view.View
+import android.view.WindowManager
+import android.widget.PopupWindow
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
 
 class MyActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my)
+
+        setupRed()
+        setupBlue()
+        setupOrange()
+        setupGreen()
     }
+
+    private fun setupRed() {
+        val redInfoButton = findViewById<View>(R.id.redInfo)
+        setupPopupButton(
+            redInfoButton,
+            R.layout.red_popup
+        ) { buttonPosition: IntArray, popupSize: Size ->
+            Point(
+                if (redInfoButton.isInRtl()) {
+                    buttonPosition.x
+                } else {
+                    buttonPosition.x + redInfoButton.width - popupSize.width
+                },
+                buttonPosition.y + redInfoButton.height
+            )
+        }
+    }
+
+    private fun View.isInRtl(): Boolean {
+        return ViewCompat.getLayoutDirection(this) == ViewCompat.LAYOUT_DIRECTION_RTL
+    }
+
+    private fun setupBlue() {
+        val blueInfoButton = findViewById<View>(R.id.blueInfo)
+        setupPopupButton(
+            blueInfoButton,
+            R.layout.blue_popup
+        ) { buttonPosition: IntArray, popupSize: Size ->
+            Point(
+                if (blueInfoButton.isInRtl()) {
+                    buttonPosition.x + blueInfoButton.width - popupSize.width
+                } else {
+                    buttonPosition.x
+                },
+                buttonPosition.y + blueInfoButton.height
+            )
+        }
+    }
+
+    private fun setupGreen() {
+        val greenInfoButton = findViewById<View>(R.id.greenInfo)
+        setupPopupButton(
+            greenInfoButton,
+            R.layout.green_popup
+        ) { buttonPosition: IntArray, popupSize: Size ->
+            Point(
+                if (greenInfoButton.isInRtl()) {
+                    buttonPosition.x + greenInfoButton.width - popupSize.width
+                } else {
+                    buttonPosition.x
+                },
+                buttonPosition.y - popupSize.height
+            )
+        }
+    }
+
+    private fun setupOrange() {
+        val orangeInfoButton = findViewById<View>(R.id.orangeInfo)
+        setupPopupButton(
+            orangeInfoButton,
+            R.layout.orange_popup
+        ) { buttonPosition: IntArray, popupSize: Size ->
+            Point(
+                if (orangeInfoButton.isInRtl()) {
+                    buttonPosition.x
+                } else {
+                    buttonPosition.x + orangeInfoButton.width - popupSize.width
+                },
+                buttonPosition.y - popupSize.height
+            )
+        }
+    }
+
+    private fun setupPopupButton(
+        popupButton: View,
+        popupLayoutRes: Int,
+        determinePopupLocation: (buttonPosition: IntArray, popupSize: Size) -> Point
+    ) {
+        val popupView = View.inflate(this, popupLayoutRes, null)
+        // we need to measure it so we can use the dimens in our calc to position on screen
+        popupView.measure(
+            View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED),
+            View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED)
+        )
+        val popupSize = Size(popupView.measuredWidth, popupView.measuredHeight)
+
+        val popupWindow = PopupWindow(
+            popupView,
+            WindowManager.LayoutParams.WRAP_CONTENT,
+            WindowManager.LayoutParams.WRAP_CONTENT,
+            true
+        ).also {
+            it.elevation = popupView.elevation
+        }
+
+        popupButton.setOnClickListener {
+            val buttonPosition = IntArray(2)
+            popupButton.getLocationOnScreen(buttonPosition)
+            val location = determinePopupLocation(buttonPosition, popupSize)
+            popupWindow.showAtLocation(
+                popupButton,
+                Gravity.NO_GRAVITY,
+                location.x,
+                location.y
+            )
+        }
+    }
+
+    private val IntArray.x
+        get() = this[0]
+
+    private val IntArray.y
+        get() = this[1]
+
+    private val Int.dp
+        get() = (this * Resources.getSystem().displayMetrics.density).toInt()
 }
+
