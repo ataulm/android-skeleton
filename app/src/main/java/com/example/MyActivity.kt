8791b972ec1072749c72d415ca5480d7e35c319d
Ataul Munim <hello@ataulm.com>
2021-10-30 15:47:08 +0100

    Update to use compose

--- app/src/main/java/com/example/MyActivity.kt
+++ app/src/main/java/com/example/MyActivity.kt
@@ -1,12 +1,51 @@
 package com.example
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.ui.theme.AndroidskeletonTheme
 
-class MyActivity : AppCompatActivity() {
-
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_my)
+//        setContentView(R.layout.activity_my)
+        setContent {
+            AndroidskeletonTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Button(
+                        onClick = {},
+                        modifier = Modifier.wrapContentSize()
+                    ) {
+                        Text(text = "I'm a compose button!")
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    AndroidskeletonTheme {
+        Greeting("Android")
     }
 }
