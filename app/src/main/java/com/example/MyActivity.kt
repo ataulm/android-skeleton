bb57d01c994425039c296a55db017e5e38a3dff9
Ataul Munim <hello@ataulm.com>
2022-01-22 13:33:30 +0000

    Add fizzbuzz example

--- app/src/main/java/com/example/MyActivity.kt
+++ app/src/main/java/com/example/MyActivity.kt
@@ -3,13 +3,16 @@ package com.example
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.ui.theme.AndroidskeletonTheme
@@ -25,12 +28,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Button(
-                        onClick = {},
-                        modifier = Modifier.wrapContentSize()
-                    ) {
-                        Text(text = "I'm a compose button!")
-                    }
+                    Screen()
                 }
             }
         }
@@ -38,14 +36,54 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Screen() {
+    val text = remember { mutableStateOf("") }
+    val numbers = IntRange(1, 100).toList()
+    LazyColumn {
+        item {
+            TextField(
+                value = text.value,
+                onValueChange = {
+                    text.value = it
+                },
+                placeholder = {
+                    Text("Type here")
+                }
+            )
+        }
+        val filtered = when (text.value) {
+            "" -> numbers
+            "fizz" -> numbers.fizz()
+            "buzz" -> numbers.buzz()
+            "fizzbuzz" -> numbers.fizzbuzz()
+            else -> emptyList()
+        }
+        if (filtered.isNotEmpty()) {
+            items(filtered) {
+                Text("Number $it")
+            }
+        } else {
+            item {
+                Column(
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier.fillParentMaxHeight()
+                ) {
+                    Text("Nope!")
+                    Text("You gotta type fizz, buzz or fizzbuzz!")
+                }
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    AndroidskeletonTheme {
-        Greeting("Android")
-    }
+private fun List<Int>.fizz(): List<Int> {
+    return filter { it % 3 == 0 }
+}
+
+private fun List<Int>.buzz(): List<Int> {
+    return filter { it % 5 == 0 }
+}
+
+private fun List<Int>.fizzbuzz(): List<Int> {
+    return filter { it % 3 == 0 || it % 5 == 0 }
 }
